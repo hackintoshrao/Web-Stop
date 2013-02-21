a80204aa92e5362d347620e48cb1a9836515c857
Karthic Rao <kartronics85@gmail.com>
2013-02-21 10:42:51 +0530

    setting up the map

--- loc.js
+++ loc.js
@@ -1,5 +1,41 @@
+var showMap;
+
+var ourCoords = { 
+	latitude:13.3500 ,
+	longitude :74.7500
+};
+
 window.onload = getLocation ;
 
+function showMap(coords) {
+	var googleLatAndLong = new google.maps.LatLng(coords.latitude,coords.longitude);
+	var mapOptions	= {
+		zoom:10, 
+		centre:googleLatAndLong ,
+		mapTypeId:google.maps.MapTypeId.ROADMAP	
+	};
+	var mapDiv = document.getElementById("map");
+	map = new google.mps.Map(mapDiv,mapOptions);
+}
+
+
+
+function computeDistance(startCoords, destCoords) {
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+	var Radius = 6371; // radius of the Earth in km
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) +
+Math.cos(startLatRads) * Math.cos(destLatRads) *
+Math.cos(startLongRads - destLongRads)) * Radius;
+	return distance ;
+}
+function degreesToRadians(degrees) {
+var radians = (degrees * Math.PI)/180;
+
+return radians;
+}
 function getLocation() {
 	if(navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(displayLocation);
@@ -14,4 +50,10 @@ function displayLocation(position) {
 
 	var div = document.getElementById("Location");
 	div.innerHTML = "You are at latitude: " + latitude + " Longitude: " + longitude;
+	var km = computeDistance(position.coords, ourCoords);
+	var distance = document.getElementById("distance");
+	distance.innerHTML = "Distance: " + km ;
+	showMap(position.coords);
 }
+
+
