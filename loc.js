0a39f50c3555b0b825e42f70d0ce9f3d5ef2927e
Karthic Rao <kartronics85@gmail.com>
2013-03-03 20:10:31 +0530

    Added the marker

--- loc.js
+++ loc.js
@@ -7,8 +7,29 @@ var ourCoords = {
 
 window.onload = getLocation ;
 
+function addMarker(map,google_latLng,title,content) {
+	var markerOptions = { 
+		position: google_latLng,
+		map: map,
+		title: title , 
+		clickable: true 
+ 	};
+	var Info_Window_Options = { 
+		content : content , 
+		position : google_latLng 
+	};
+	var marker = new google.maps.Marker(markerOptions);
+	var infoWindow = new google.maps.InfoWindow(Info_Window_Options);
+	google.maps.event.addListener(marker,"click",function() {
+		infoWindow.open(map);
+	});
+}	
+	
+
 function showMap(coords) {
 	var googleLatAndLong = new google.maps.LatLng(coords.latitude,coords.longitude);
+	var title_for_pin = "Here you are !!";
+  	var content_for_click = "Your Location: " +" " + "Latitude: " + coords.latitude + " " + "Longitude: " +  coords.longitude ; 
 	var mapOptions	= {
 		zoom: 10, 
 		center: googleLatAndLong ,
@@ -16,7 +37,8 @@ function showMap(coords) {
 	};
 	var mapDiv = document.getElementById("map");
 	map = new google.maps.Map(mapDiv,mapOptions);
-}
+	addMarker(map,googleLatAndLong,title_for_pin,content_for_click);
+}	
 
 
 
